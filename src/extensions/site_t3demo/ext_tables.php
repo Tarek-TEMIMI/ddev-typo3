517f7135498be72d44408cbe83d8ed2b59a9b1c6
David Steeb <david.steeb@b13.com>
2022-10-11 08:20:01 +0200

    [TASK] Add EXT:doktypemapper
    Related: #69

--- src/extensions/site_t3demo/ext_tables.php
+++ src/extensions/site_t3demo/ext_tables.php
@@ -3,11 +3,6 @@
 defined('TYPO3_MODE') or die();
 
 (function () {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] =
-        \B13\SiteT3demo\Hooks\Datahandler::class;
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] =
-        \B13\SiteT3demo\Hooks\Datahandler::class;
-
     $GLOBALS['TBE_STYLES']['skins']['site_t3demo']['name'] = 'site_t3demo';
     $GLOBALS['TBE_STYLES']['skins']['site_t3demo']['stylesheetDirectories'] = [
         'EXT:site_t3demo/Resources/Public/Backend/Css/Skin/',
